80419a7caa83e99cacbe46685893f0704ba6ea31
BIDDI Rachid <a521rr@login.axa>
2023-05-22 12:28:32 +0200

    V0

--- src/main/java/com/ocs/safetynet/controller/TestController.java
+++ src/main/java/com/ocs/safetynet/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.example.projet5.controler;
+package com.ocs.safetynet.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
