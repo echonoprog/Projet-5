c5b9b56ea7bb7e128cb87df3bcef9cc8cec93edd
BIDDI Rachid <a521rr@login.axa>
2023-07-20 15:22:29 +0200

    Add logger

--- src/main/java/com/ocs/safetynet/controller/FirestationController.java
+++ src/main/java/com/ocs/safetynet/controller/FirestationController.java
@@ -4,16 +4,20 @@ package com.ocs.safetynet.controller;
 import com.ocs.safetynet.dto.FirestationStationNumberCountDto;
 import com.ocs.safetynet.model.Firestation;
 import com.ocs.safetynet.service.FirestationService;
-
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
+
 @RestController
 public class FirestationController {
+    private final Logger logger = LoggerFactory.getLogger(FirestationController.class);
+
     private FirestationService firestationService;
 
     public FirestationController(FirestationService firestationService) {
@@ -22,13 +26,14 @@ public class FirestationController {
 
     @GetMapping("/firestation")
     public List<Firestation> getAllFirestations() {
-
+        logger.info("All Firestations");
         return firestationService.getAllFirestations();
 
     }
 
     @PostMapping("firestation")
     public ResponseEntity<Firestation> addFirestation(@RequestBody Firestation firestation) {
+        logger.info("Add Firestation");
         Firestation savedFirestation = firestationService.addFirestation(firestation);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedFirestation);
 
@@ -36,6 +41,7 @@ public class FirestationController {
 
     @PutMapping("/firestation/{address}&{station}")
     public ResponseEntity<Firestation> updateFirestation(@PathVariable String address, @PathVariable String station, @RequestBody Firestation firestation) {
+        logger.info("Update Firestation");
         Firestation updatedFirestation = firestationService.updateFirestation(address, station, firestation);
         if (updatedFirestation != null) {
             return ResponseEntity.status(HttpStatus.OK).body(updatedFirestation);
@@ -47,6 +53,7 @@ public class FirestationController {
 
     @DeleteMapping("/firestation/{address}&{station}")
     public ResponseEntity<Void> deleteFirestation(@PathVariable String address, @PathVariable String station) {
+        logger.info("Delete Firestation");
         firestationService.deleteFirestation(address, station);
         return ResponseEntity.noContent().build();
     }
@@ -54,6 +61,7 @@ public class FirestationController {
     /** Endpoint pour récupérer les informations des personnes couvertes par la caserne de pompiers correspondante **/
     @GetMapping("/firestation/{stationNumber}")
     public ResponseEntity<FirestationStationNumberCountDto> getFirestationCoverageByStationNumber(@PathVariable  String stationNumber) {
+        logger.info("Coverage By Station Number");
         FirestationStationNumberCountDto firestationCoverage = firestationService.getFirestationCoverageByStationNumber(stationNumber);
         if (firestationCoverage != null) {
             return ResponseEntity.status(HttpStatus.OK).body(firestationCoverage);
--- src/main/java/com/ocs/safetynet/controller/PersonController.java
+++ src/main/java/com/ocs/safetynet/controller/PersonController.java
@@ -4,6 +4,8 @@ package com.ocs.safetynet.controller;
 import com.ocs.safetynet.dto.*;
 import com.ocs.safetynet.model.Person;
 import com.ocs.safetynet.service.PersonService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +16,7 @@ import java.util.List;
 
 public class PersonController {
 
+    private final Logger logger = LoggerFactory.getLogger(PersonController.class);
 
     private final PersonService personService;
 
@@ -23,6 +26,7 @@ public class PersonController {
 
     @GetMapping("/person")
     public List<Person> getAllPersons() {
+        logger.info("All Persons");
         return personService.getAllPersons();
 
     }
@@ -30,24 +34,28 @@ public class PersonController {
 
     @PostMapping("/person")
     public ResponseEntity<Person> addPerson(@RequestBody Person person) {
+        logger.info("Add Person");
         Person addedPerson = personService.addPerson(person);
         return ResponseEntity.status(HttpStatus.CREATED).body(addedPerson);
     }
 
     @PutMapping("/person/{firstName}&{lastName}")
     public ResponseEntity<Person> updatePerson(@PathVariable String firstName, @PathVariable String lastName, @RequestBody Person person) {
+        logger.info("Update Person");
         Person updatedPerson = personService.updatePerson(firstName, lastName, person);
         return ResponseEntity.ok(updatedPerson);
     }
 
     @DeleteMapping("/person/{firstName}&{lastName}")
     public ResponseEntity<Void> deletePerson(@PathVariable String firstName, @PathVariable String lastName) {
+        logger.info("Delete Person");
         personService.deletePerson(firstName, lastName);
         return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/childAlert/{address}")
     public ResponseEntity<List<ChildAlertDto>> getChildAlert(@PathVariable String address) {
+        logger.info("List of children living at the given address");
         List<ChildAlertDto> childrenAtAddress = personService.getChildrenAtAddress(address);
         if (childrenAtAddress.isEmpty()) {
             return ResponseEntity.noContent().build();
@@ -58,6 +66,7 @@ public class PersonController {
 
     @GetMapping("/phoneAlert/{firestation}")
     public ResponseEntity<List<PhoneAlertFirestationDto>> getPhoneNumbersByFirestation(@PathVariable("firestation") int firestationNumber) {
+        logger.info("List of phone numbers of residents served by the fire station");
         List<PhoneAlertFirestationDto> phoneNumbers = personService.getPhoneNumbersByFirestation(firestationNumber);
         if (phoneNumbers.isEmpty()) {
             return ResponseEntity.noContent().build();
@@ -68,6 +77,7 @@ public class PersonController {
 
     @GetMapping("/fire/{address}")
     public ResponseEntity<List<FireAddressDto>> getFireAddressDetails(@PathVariable("address") String address) {
+        logger.info("List of residents living at the given address and the number of the fire station serving it");
         List<FireAddressDto> fireAddressDetails = personService.getFireAddressDetails(address);
         if (fireAddressDetails.isEmpty()) {
             return ResponseEntity.noContent().build();
@@ -78,6 +88,7 @@ public class PersonController {
 
     @GetMapping("/flood/{stations}")
     public ResponseEntity<List<FloodStationDto>> getFloodDetailsByStations(@PathVariable("stations") List<Integer> firestationNumbers) {
+        logger.info("List of all households served by the specified fire stations, grouped by address");
         List<FloodStationDto> floodStationDetails = personService.getFloodDetailsByStations(firestationNumbers);
         if (floodStationDetails.isEmpty()) {
             return ResponseEntity.noContent().build();
@@ -88,6 +99,7 @@ public class PersonController {
 
     @GetMapping("/personInfo/{firstName}&{lastName}")
     public ResponseEntity<List<PersoninfoNameDto>> getPersonInfo(@PathVariable String firstName, @PathVariable String lastName) {
+        logger.info("Information of residents based on first name and last name");
         List<PersoninfoNameDto> personInfoList = personService.getPersonInfoByName(firstName, lastName);
         if (personInfoList.isEmpty()) {
             return ResponseEntity.noContent().build();
@@ -98,6 +110,7 @@ public class PersonController {
 
     @GetMapping("/communityEmail/{city}")
     public ResponseEntity<List<CommunityemailDto>> getEmailsByCity(@PathVariable String city) {
+        logger.info("Email addresses of all residents in the city");
         List<CommunityemailDto> emails = personService.getEmailsByCity(city);
         if (emails.isEmpty()) {
             return ResponseEntity.noContent().build();
